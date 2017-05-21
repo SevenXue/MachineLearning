7ac33b8503b32cf45b9b166dc3867678605f8d61
SevenXue <825056849@qq.com>
2017-05-21 20:32:21 +0800

    Kmeans

---
+++ Unsupervised/Kmeans.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+digits_train = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/optdigits/optdigits.tra', header=None)
+
+digits_test = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/optdigits/optdigits.tes', header=None)
+
+x_train = digits_train[np.arange(64)]
+
+Y_train =digits_train[64]
+
+x_test = digits_test[np.arange(64)]
+y_test = digits_test[64]
+
+from sklearn.cluster import KMeans
+
+kmeans = KMeans(n_clusters=10)
+kmeans.fit(x_train)
+
+y_predict = kmeans.predict(x_test)
+
+from sklearn import metrics
+
+print(metrics.adjusted_rand_score(y_test, y_predict))
+
+
+
+
