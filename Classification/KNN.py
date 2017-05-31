55c3b2795adfdb71dba04aa40d907879f2e379d8
SevenXue <825056849@qq.com>
2017-05-31 22:37:42 +0800

    KNN
    修改备注

--- Classification/KNN.py
+++ Classification/KNN.py
@@ -8,7 +8,7 @@ from sklearn.datasets import load_iris
 iris = load_iris()
 
 print(iris.data.shape)
-print(iris.DESCR)
+print(iris.DESCR) #！查看数据说明
 
 from sklearn.cross_validation import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.25, random_state=38)
