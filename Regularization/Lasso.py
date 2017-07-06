810204e75fa79b28c6ef92a6c267f1959814f38f
SevenXue <825056849@qq.com>
2017-07-06 22:13:03 +0800

    Lasso
    lasso

---
+++ Regularization/Lasso.py
@@ -0,0 +1,58 @@
+#~ L1范数正则化
+#~ 使用的数据为pizza价格
+
+x_train = [[6], [8], [10], [14], [18]]
+y_train = [[7], [9], [13], [17.5], [18]]
+
+x_test = [[6], [8], [11], [16]]
+y_test = [[8], [12], [15], [18]]
+
+from sklearn.linear_model import LinearRegression
+#导入多项式特征产生器
+from sklearn.preprocessing import PolynomialFeatures
+
+#degree=4 表示初始化2,4次多项式特征生成器
+poly2 = PolynomialFeatures(degree=2)
+x_train_poly2 = poly2.fit_transform(x_train)
+
+poly4 = PolynomialFeatures(degree=4)
+x_train_poly4 = poly4.fit_transform(x_train)
+
+#线性回归
+regress_poly2 = LinearRegression()
+regress_poly2.fit(x_train_poly2, y_train)
+
+regress_poly4 = LinearRegression()
+regress_poly4.fit(x_train_poly4, y_train)
+
+x_test_poly2 = poly2.transform(x_test)
+x_test_poly4 = poly4.transform(x_test)
+print(regress_poly2.score(x_test_poly2, y_test))
+print(regress_poly4.score(x_test_poly4, y_test))
+
+#绘图
+import numpy as np
+xx = np.linspace(0, 26, 100)
+xx = xx.reshape(xx.shape[0], 1)
+# print(xx.shape)
+xx_poly2 = poly2.transform(xx)
+xx_poly4 = poly4.transform(xx)
+yy_poly2 = regress_poly2.predict(xx_poly2)
+yy_poly4 = regress_poly4.predict(xx_poly4)
+
+import matplotlib.pyplot as plt
+plt.scatter(x_train, y_train)
+plt2, = plt.plot(xx, yy_poly2, label='Degree2')
+plt4, = plt.plot(xx, yy_poly4, label='Degree4')
+
+plt.axis([0, 25, 0, 25])
+plt.xlabel('Diameter of Pizza')
+plt.ylabel('Price of Pizza')
+plt.legend(handles=[plt2, plt4])
+plt.show()
+
+#lasso
+from sklearn.linear_model import Lasso
+lasso_poly4 = Lasso()
+lasso_poly4.fit(x_train_poly4, y_train)
+print(lasso_poly4.score(x_test_poly4, y_test))
--- feature/SelectFeature.py
+++ feature/SelectFeature.py
@@ -9,7 +9,7 @@ titanic.info()  #!查看数据的统计信息
 
 #分离数据特征与预测目标
 y = titanic['survived']
-x = titanic.drop(['row.name', 'name', 'survived'], axis=1)
+x = titanic.drop(['row.names', 'name', 'survived'], axis=1)
 
 #对缺失数据进行填充
 x['age'].fillna(x['age'].mean(), inplace=True)
@@ -24,6 +24,51 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random
 from sklearn.feature_extraction import DictVectorizer
 vec = DictVectorizer()
 x_train =  vec.fit_transform(x_train.to_dict(orient='record'))
-x_test = vec.transform(x_test.to_dict(orinet='record'))
+x_test = vec.transform(x_test.to_dict(orient='record'))
 
-print(len(vec.feature_names_))
+#print(len(vec.feature_names_))
+
+#使用决策树模型进行预测
+from sklearn.tree import DecisionTreeClassifier
+dt = DecisionTreeClassifier(criterion='entropy')
+dt.fit(x_train, y_train)
+print(dt.score(x_test, y_test)) #0.8115
+
+#从sklearn导入特征筛选器
+from sklearn import feature_selection
+
+'''
+#筛选前20%的特征
+fs = feature_selection.SelectPercentile(feature_selection.chi2, percentile=20)
+x_train_fs = fs.fit_transform(x_train, y_train)
+dt.fit(x_train_fs, y_train)
+x_test_fs = fs.transform(x_test)
+print(dt.score(x_test_fs, y_test))
+'''
+
+#通过交叉验证，按照固定间隔的百分比筛选特征，并作图展示
+from sklearn.cross_validation import cross_val_score
+import numpy as np
+
+percentiles = range(1, 100, 2)
+results = []
+
+for i in percentiles:
+    fs = feature_selection.SelectPercentile(feature_selection.chi2, percentile=i)
+    x_train_fs = fs.fit_transform(x_train, y_train)
+    scores = cross_val_score(dt, x_train_fs, y_train, cv=5)
+    results.append(scores.mean())
+print(results)
+
+import pylab as pl
+pl.plot(percentiles, results)
+pl.xlabel('percentiles of features')
+pl.ylabel('accuracy')
+pl.show()
+
+#使用最佳筛选后的特征，进行测试
+fsbest = feature_selection.SelectPercentile(feature_selection.chi2, percentile=7)
+x_train_fsbest = fsbest.fit_transform(x_train, y_train)
+dt.fit(x_train_fsbest, y_train)
+x_test_fsbest = fsbest.transform(x_test)
+dt.score(x_test_fsbest, y_test)
